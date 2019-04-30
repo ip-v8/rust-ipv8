4d9a67537741cb18f53d7e0fe81aeb32493f8887
Dany Sluijk <dany@atlasdev.nl>
2019-04-30 12:48:47 +0200

    feat: create the initial library files

---
+++ examples/join.rs
@@ -0,0 +1,11 @@
+/**
+ * In this example the program attempts to join the network.
+ *
+ * It does not do much, except for logging the output.
+ */
+
+use ipv8;
+
+fn main() {
+  let _ = ipv8::new();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
+/**
+ * Create a new instance of IPv8.
+ */
+pub fn new() -> IPv8 {
+  return IPv8 {};
+}
 
+/**
+ * The IPv8 instance.
+ *
+ * This struct is how you can interact with the network.
+ */
+pub struct IPv8 {}
