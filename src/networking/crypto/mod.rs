29f11849fb61ccab4f17812cf148bce670489ad9
Victor Roest <victor@xirion.net>
2019-05-24 22:54:25 +0200

    Added some boundary tests to expect errors

--- src/networking/crypto/mod.rs
+++ src/networking/crypto/mod.rs
@@ -51,3 +51,52 @@ pub fn verify_signature_openssl(signature: (BigNum, BigNum), data: &[u8], pkey: 
     Err(_) => return Err(Box::new(OpenSSLError)) // Should **never** happen but if it does openssl burn it
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use crate::networking::crypto::{verify_signature_ed25519, verify_signature_openssl, SizeError, create_signature_openssl};
+  use rust_sodium::crypto::sign::ed25519;
+  use openssl::bn::BigNum;
+  use std::os::raw::c_int;
+  use std::error::Error;
+  use std::any::Any;
+
+
+  #[test]
+  fn ed25519_verify_signature_error() {
+    let seed = ed25519::Seed::from_slice(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,]).unwrap();
+    let (pkey,skey) = ed25519::keypair_from_seed(&seed);
+
+    match verify_signature_ed25519(vec![42], &vec![42], pkey) {
+      Ok(_) => assert!(false, "This shouldn't happen as the signature is malformed thus we expect an error"),
+      Err(_) => assert!(true)
+    }
+  }
+
+  #[test]
+  fn openssl_verify_signature_size_error() {
+
+    let r = BigNum::new().unwrap();
+    let s = BigNum::new().unwrap();
+
+    let tmp:Vec<u8> = vec![0; (c_int::max_value() as usize) + 1];
+    let pkey_tmp = openssl::pkey::PKey::public_key_from_pem("-----BEGIN PUBLIC KEY-----\nMEAwEAYHKoZIzj0CAQYFK4EEAAEDLAAEAFDvrGilTKwG5YicaRf5Lh6UV2k5BmmGAuVzqSyiKb7kOBRkQE+n4HYO\n-----END PUBLIC KEY-----".as_bytes()).unwrap();
+    let pkey = pkey_tmp.ec_key().unwrap();
+
+    match verify_signature_openssl((r,s), &tmp,  pkey) {
+      Ok(_) => assert!(false, "We expect failure as the data is too big"),
+      Err(e) => assert_eq!(e.description(), Box::new(SizeError).description())
+    }
+  }
+
+  #[test]
+  fn openssl_create_signature_size_error() {
+    let skey = openssl::ec::EcKey::private_key_from_pem("-----BEGIN EC PRIVATE KEY-----\nMFMCAQEEFQKu4aaDxyTSj92iquQP5CIdbagLP6AHBgUrgQQAAaEuAywABABQ76xopUysBuWInGkX+S4elFdpOQZphgLlc6ksoim+5DgUZEBPp+B2Dg==\n-----END EC PRIVATE KEY-----".as_bytes()).unwrap();
+    let tmp:Vec<u8> = vec![0; (c_int::max_value() as usize) + 1];
+
+    match create_signature_openssl(&tmp, skey) {
+      Ok(_) => assert!(false, "We expect failure as the data is too big"),
+      Err(e) => assert_eq!(e.description(), Box::new(SizeError).description())
+    }
+  }
+}
